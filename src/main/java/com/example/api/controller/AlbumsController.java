0fd5ad31ed905de661dc3b0b4457e09ae0c29528
Hannia237 <hch06263@gmail.com>
2022-10-12 12:16:35 -0600

    Videos 7-9

---
+++ src/main/java/com/example/api/controller/AlbumsController.java
@@ -0,0 +1,23 @@
+package com.example.api.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.api.entity.Album;
+import com.example.api.service.IAlbumsService;
+
+@RestController
+@RequestMapping("/api")
+public class AlbumsController {
+	
+	@Autowired
+	private IAlbumsService serviceAlbums;
+	
+	@GetMapping("/albums")
+	public List<Album> buscarTodos() {
+		return serviceAlbums.buscarTodos();
+	}
+
+}
